96d911c5c4f1317936b3165149c9f60da8c57b8d
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-16 15:18:17 -0700

    Create todo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_220041) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_221640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "to_dos", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
