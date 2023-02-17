173cb9467fa38cffaa67b9514d94aa611d889882
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-17 11:11:34 -0700

    Add todo model validations

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,4 +1,13 @@
 class ToDo < ApplicationRecord
     has_many :todo_categories
     has_many :categories, through: :todo_categories
+
+    validates :title, uniqueness: {
+        message: 'This task has already been created before'
+    }
+    validates :title, presence: true
+    validates :title, length: {
+        minimum: 3,
+        too_short: 'Task name must have a minimum of %{count} characters'
+    }
 end
