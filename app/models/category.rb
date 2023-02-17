ef471cf983a8e2ae87e36cafa02384f746b7bae6
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-17 11:11:52 -0700

    Add category model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
 class Category < ApplicationRecord
     has_many :todo_categories
     has_many :to_dos, through: :todo_categories
+
+    validates :title, uniqueness: {
+        message: 'already exists'
+    }
+    validates :title, presence: true
+    validates :title, length: {
+        minimum: 3,
+        too_short: 'must have a minimum of %{count} characters'
+    }
 end
