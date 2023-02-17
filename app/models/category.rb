066d029e517228cc9e07c5a6ca9ade153507e038
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-17 10:47:09 -0700

    Add todo category relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :todo_categories
+    has_many :to_dos, through: :todo_categories
 end
--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,2 +1,4 @@
 class ToDo < ApplicationRecord
+    has_many :todo_categories
+    has_many :categories, through: :todo_categories
 end
